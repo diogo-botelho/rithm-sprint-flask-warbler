cde63c5432c7a7c81a9133821b94dd144c57b1eb
Diogo <diogofilipebotelho@gmail.com>
2021-09-23 20:05:30 -0400

    Added tests for __repr__, login and add_following

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,7 @@
 
 import os
 from unittest import TestCase
+from flask import session, g
 
 from models import db, User, Message, Follows
 
@@ -19,7 +20,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 # Now we can import app
 
-from app import app
+from app import app, CURR_USER_KEY
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -30,13 +31,15 @@ db.create_all()
 TEST_USER_DATA_1 = {
     "email":"test@test.com",
     "username":"testuser",
-    "password":"HASHED_PASSWORD"
+    "password":"HASHED_PASSWORD",
+    "image_url":""
 }
 
 TEST_USER_DATA_2 = {
     "email":"test2@test.com",
     "username":"testuser2",
-    "password":"HASHED_PASSWORD"
+    "password":"HASHED_PASSWORD",
+    "image_url":""
 }
 
 class UserModelTestCase(TestCase):
@@ -49,13 +52,13 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         User.query.delete()
 
-        test_user_1 = User(**TEST_USER_DATA_1)
-        test_user_2 = User(**TEST_USER_DATA_2)
+        test_user_1 = User.signup(**TEST_USER_DATA_1)
+        test_user_2 = User.signup(**TEST_USER_DATA_2)
         db.session.add_all([test_user_1, test_user_2])
         db.session.commit()
 
-        self.test_user_1 = test_user_1
-        self.test_user_2 = test_user_2
+        self.test_user_1_id = test_user_1.id
+        self.test_user_2_id = test_user_2.id
 
         self.client = app.test_client()
     
@@ -67,10 +70,47 @@ class UserModelTestCase(TestCase):
     def test_user_model(self):
         """Does basic model work?"""
 
-        # User1 should have no messages & no followers
-        self.assertEqual(len(self.test_user_1.messages), 0)
-        self.assertEqual(len(self.test_user_1.followers), 0)
+        test_user_1 = User.query.get(self.test_user_1_id)
 
-        # User2 should have no messages & no followers
-        self.assertEqual(len(self.test_user_2.messages), 0)
-        self.assertEqual(len(self.test_user_2.followers), 0)
+        # User1 should have no messages & no followers
+        self.assertEqual(len(test_user_1.messages), 0)
+        self.assertEqual(len(test_user_1.followers), 0)
+        self.assertEqual(User.query.count(),2)
+
+    def test_repr_method(self):
+        """Test that __repr__ works"""
+
+        test_user_1 = User.query.get(self.test_user_1_id)        
+        
+        self.assertEqual(f"{test_user_1}",f"<User #{test_user_1.id}: {test_user_1.username}, {test_user_1.email}>")
+
+    def test_login_works(self):
+        """Test if login works."""
+
+        url = '/login'
+        resp = self.client.post(url,json = {"username": "test_user", "password": "HASHED_PASSWORD"},follow_redirects=True)
+
+        html = resp.get_data(as_text=True)
+
+        self.assertEqual(resp.status_code,200)
+        breakpoint()
+        self.assertIn('<h2 class="join-message">Welcome back.</h2>', html)
+        # self.assertEqual(session[CURR_USER_KEY],self.test_user_1_id)
+
+
+    def test_is_following(self):
+        """Test user1 is following user2"""
+
+        with self.client.session_transaction() as sess:
+            sess[CURR_USER_KEY] = self.test_user_1_id
+
+        url = f"/users/follow/{self.test_user_2_id}"
+        resp = self.client.post(url,follow_redirects=True)
+        html = resp.get_data(as_text=True)
+
+        self.assertEqual(resp.status_code,200)
+        self.assertIn('<p>@testuser2</p>',html)
+        self.assertEqual(Follows.query.count(), 1)
+
+
+    # def test_is_not_following()
